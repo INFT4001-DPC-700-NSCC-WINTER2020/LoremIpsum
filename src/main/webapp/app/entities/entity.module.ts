d94d8faf343a0c78ecf2dae84f2828d6c57ade73
w0404844 <ronan.o'driscoll@nscc.ca>
2020-02-03 09:32:30 -0400

    Entity Created. Fixed #3

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'points',
+        loadChildren: () => import('./points/points.module').then(m => m.TwentyOnePointsPointsModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ]
